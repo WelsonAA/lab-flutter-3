e02ba380fc72626420eb8ea7dcb0db83d12c9cd3
WelsonAA <20p3831@eng.asu.edu.eg>
2024-11-30 22:07:12 +0200

    safer

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,7 @@ import 'screens/card_creation_screen.dart';
 import 'screens/card_viewer_screen.dart';
 import 'screens/home_screen.dart';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  'apiKey': "AIzaSyALENaO_mqXJqhAhrrAmox2u20ihfmyIxk",
-  'authDomain': "flutter3-6ac21.firebaseapp.com",
-  'projectId': "flutter3-6ac21",
-  'storageBucket': "flutter3-6ac21.firebasestorage.app",
-  'messagingSenderId': "661726668698",
-  'appId': "1:661726668698:web:3bef2c4c61c791995b7c23",
-  'measurementId': "G-YFGX8BDW8J"
-};
+
 
 void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -31,23 +22,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Business Card App',
       theme: ThemeData(
-        // Primary color set to purple
-        primaryColor: Colors.purple,
-        // Use colorScheme to manage the secondary color (white for contrast)
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple).copyWith(secondary: Colors.white),
+        // Primary color set to white
+        primaryColor: Colors.white,
+        // Use colorScheme to manage the secondary color
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white).copyWith(secondary: Colors.blue),
         // Default background color set to white
         scaffoldBackgroundColor: Colors.white,
-        // Text theme (set text to black to contrast with the white background)
-
-        // AppBar settings with purple background and white text
+        // Text theme (set text to white to contrast with the white background)
+        
+        // AppBar settings to have a white background with blue icons/text
         appBarTheme: AppBarTheme(
-          color: Colors.purple,  // Purple AppBar background
-          iconTheme: IconThemeData(color: Colors.white),  // White icons in AppBar
-          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),  // White text in AppBar
+          color: Colors.white,  // white AppBar background
+          iconTheme: IconThemeData(color: Colors.blue),  // blue icons
+          titleTextStyle: TextStyle(color: Colors.blue, fontSize: 20),  // blue text in AppBar
         ),
-        // FloatingActionButton theme with purple background
+        // FloatingActionButton theme to match the color scheme
         floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.purple,  // Purple background for FABs
+          backgroundColor: Colors.blue,  // blue background for FABs
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
